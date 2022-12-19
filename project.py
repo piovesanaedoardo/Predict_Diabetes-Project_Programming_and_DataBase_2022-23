413764c69fe53ad5c36ba5050b2a9c1bef3b836f
Edoardo Piovesana <piovesana.edoardo@gmail.com>
2022-12-19 18:08:45 +0100

    add streamlit

--- project.py
+++ project.py
@@ -2,4 +2,11 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 
+# streamlit run .\project.py
+
 mXmh_df = pd.read_csv('mxmh_survey_results.csv')
+
+mXmh_df.head(10)
+
+st.header('Music & Mental Healt')
+st.subheader('Survey results on music taste and self-reported mental health')
