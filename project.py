60456703b3a80162c20c26b34114b5c218b9b865
Edoardo Piovesana <piovesana.edoardo@gmail.com>
2022-12-19 21:58:11 +0100

    add pie chart

--- project.py
+++ project.py
@@ -46,7 +46,7 @@ if st.sidebar.checkbox('Display DataFrame'):
     st.write(mXmh_df)
 
 st.subheader('Plots')
-# print(mXmh_df['Streaming_Platform'].value_counts())
+# print('Value counts:', mXmh_df['Streaming_Platform'].value_counts())
 
 # st.write(plt.pie(mXmh_df['Streaming_Platform'].value_counts(), labels=mXmh_df['Streaming_Platform']))
 
@@ -62,8 +62,18 @@ st.caption('Age distribution')
 
 # with col2:
 fig, ax = plt.subplots()
-mXmh_df.Streaming_Platform.hist(ax=ax, figsize=(15,8))
+mXmh_df.Streaming_Platform.hist(ax=ax, figsize=(12,8))
 plt.xlabel('Platoform')
 plt.ylabel('Number of Users')
 st.write(fig)
+st.caption('Streaming_Platform distribution')
+
+platform_list = ['Spotify', 'YouTube Music', 'I do not use a streaming service.', 'Apple Music', 'Other streaming service', 'Pandora']
+platform_count = list(mXmh_df['Streaming_Platform'].value_counts())
+explode = (0.1, 0, 0, 0, 0, 0)  # only "explode" the 1st slice ('Spotify')
+fig1, ax1 = plt.subplots()
+ax1.pie(platform_count, explode=explode, labels=platform_list, autopct='%.2f',
+        shadow=True, startangle=90)
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+st.pyplot(fig1)
 st.caption('Streaming_Platform distribution')
