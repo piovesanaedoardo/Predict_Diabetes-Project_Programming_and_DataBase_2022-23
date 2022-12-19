b5f170b18b785bd05f305f701d5ffb7fcc3046d7
Edoardo Piovesana <piovesana.edoardo@gmail.com>
2022-12-19 18:22:14 +0100

    cleaning column age and primary streaming service

--- project.py
+++ project.py
@@ -4,10 +4,14 @@ import matplotlib.pyplot as plt
 
 # streamlit run .\project.py
 
-# DataFrame & cleaning
+# ---- DataFrame & cleaning ---- 
 mXmh_df = pd.read_csv('mxmh_survey_results.csv')
-# Replace the null values (only one) with the mean from the Age column
-mXmh_df['Age'].fillna(mXmh_df['Age'].mean())
+# Column 'Age': replacing the null values (only one) with the mean from the Age column
+mXmh_df['Age'] = mXmh_df['Age'].fillna(mXmh_df['Age'].mean())
+# Column 'Primary streaming service': replacing the null values (only one) with the string 'No one'
+mXmh_df['Primary streaming service'] = mXmh_df['Primary streaming service'].fillna('No one')
+
+mXmh_df.info()
 
 st.header('Music & Mental Healt')
 st.subheader('Survey results on music taste and self-reported mental health')
