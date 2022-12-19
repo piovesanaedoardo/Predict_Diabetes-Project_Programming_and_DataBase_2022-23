e307ba3a53770789366aa75c2dc88f906e682b3c
Edoardo Piovesana <piovesana.edoardo@gmail.com>
2022-12-19 18:17:51 +0100

    replace null values of Age column

--- project.py
+++ project.py
@@ -4,9 +4,16 @@ import matplotlib.pyplot as plt
 
 # streamlit run .\project.py
 
+# DataFrame & cleaning
 mXmh_df = pd.read_csv('mxmh_survey_results.csv')
-
-mXmh_df.head(10)
+# Replace the null values (only one) with the mean from the Age column
+mXmh_df['Age'].fillna(mXmh_df['Age'].mean())
 
 st.header('Music & Mental Healt')
 st.subheader('Survey results on music taste and self-reported mental health')
+
+st.sidebar.subheader('Settings')
+
+if st.sidebar.checkbox('Display DataFrame'):
+    st.write('The DataFrame')
+    st.write(mXmh_df)
