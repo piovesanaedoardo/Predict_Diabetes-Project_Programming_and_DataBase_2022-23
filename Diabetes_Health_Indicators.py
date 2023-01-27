49ec9f0645c433542d4b908cece39675fc266d63
Edoardo Piovesana <piovesana.edoardo@gmail.com>
2023-01-27 10:26:40 +0100

    removed useless files fixing plots

--- Diabetes_Health_Indicators.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import streamlit as st
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-diabetes_df = pd.read_csv('diabetes_binary_5050split_health_indicators_BRFSS2015.csv')
-
-# cd Documenti/Project of Programming and Database
-# streamlit run .\Diabetes_Health_Indicators.py
